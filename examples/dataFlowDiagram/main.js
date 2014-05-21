a8105a606fa4519d69569ca5257113c2aa54401d
curran <curran.kelleher@gmail.com>
2014-05-21 19:36:54 -0400

    Updated data flow diagram to respond to url hash

--- examples/dataFlowDiagram/main.js
+++ examples/dataFlowDiagram/main.js
@@ -1,10 +1,28 @@
 require(['d3', 'forceDirectedGraph'], function (d3, ForceDirectedGraph) {
   var div = document.getElementById('container'),
-      forceDirectedGraph = ForceDirectedGraph(div);
+      forceDirectedGraph = ForceDirectedGraph(div),
+      name = 'svg';
 
-  d3.json('../../dataFlowGraphs/svg.json', function (data) {
+  if(window.location.hash) {
+    name = window.location.hash.substr(1);
+  } 
+
+  d3.json('../../dataFlowGraphs/' + name + '.json', function (data) {
     forceDirectedGraph.set('data', data);
   });
+
+  function initializeZoom(){
+    var scale = 0.6;
+    forceDirectedGraph.set({
+      scale: scale,
+      translate: [
+        div.clientWidth / 2 * (1 - scale),
+        div.clientHeight / 2 * (1 - scale)
+      ]
+    });
+  }
+  initializeZoom();
+
 
   setSizeFromDiv();
   window.addEventListener('resize', setSizeFromDiv);
