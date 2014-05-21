6727d54991ebaf99ca0a96030df95204de8c8fdf
curran <curran.kelleher@gmail.com>
2014-05-20 20:08:07 -0400

    Added outputDataFlowGraph to unit tests, consumed generated data in examples/dataFlowDiagram/

--- tests/reactivisTest.js
+++ tests/reactivisTest.js
@@ -1,6 +1,7 @@
 var requirejs = require('requirejs'),
     jsdom = require('jsdom').jsdom,
     d3 = require('d3'),
+    fs = require('fs'),
     expect = require('chai').expect;
 
 requirejs.config({
@@ -12,6 +13,15 @@ requirejs.config({
   nodeRequire: require
 });
 
+function outputDataFlowGraph(name, model){
+  model.detectFlowGraph(function (graph) {
+    var json = JSON.stringify(graph, null, 2);
+    fs.writeFile('./dataFlowGraphs/svg.json', json, function(err) {
+      if(err) console.log(err);
+    }); 
+  });
+}
+
 describe('A suite', function() {
   var reactivis = requirejs('reactivis'),
       Model = requirejs('model'),
@@ -20,12 +30,28 @@ describe('A suite', function() {
   it('svg', function(done) {
     var model = Model(),
         div = document.getElementById('container');
+
+    outputDataFlowGraph('svg', model);
+
     reactivis(model).svg();
+
     model.set('div', div);
     model.when(['svg', 'g'], function(svg, g) {
       expect(svg.node().nodeName).to.equal('SVG');
       expect(g.node().nodeName).to.equal('G');
-      done();
+
+      model.set('box', {
+        x: 50,
+        y: 50,
+        width: 200,
+        height: 250
+      });
+      setTimeout(function () {
+        expect(svg.attr('width')).to.equal('200');
+        expect(svg.attr('height')).to.equal('250');
+        // TODO test x, y
+        done();
+      }, 0);
     });
   });
 });
