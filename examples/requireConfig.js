78300059e2b420782d290ec473431322a2209771
curran <curran.kelleher@gmail.com>
2014-05-22 16:02:04 -0400

    Moved everything to gh-pages branch

--- examples/requireConfig.js
+++
@@ -1,7 +0,0 @@
-require.config({
-  paths: {
-    reactivis: '../../src/reactivis',
-    model: '../../bower_components/model/dist/model',
-    d3: '../../bower_components/d3/d3.min'
-  }
-});
--- examples/scatterPlot/requireConfig.js
+++
@@ -1,8 +0,0 @@
-// The configuration for Require.js.
-// Curran Kelleher 4/17/2014
-require.config({
-  // Map the `model` module name to the model lib.
-  paths: {
-    model: '../../src/model'
-  }
-});
